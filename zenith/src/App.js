1b99ac53bef41859e1a3eb45cec890bb988efccc
xitikx <cheimonas31@gmail.com>
2025-03-14 12:56:38 +0530

    Added Amplify authentication

--- zenith/src/App.js
+++ zenith/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import awsExports from "./aws-exports";
+import "./App.css";
 
-function App() {
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Bucket List Tracker</h1>
+      <h2>Hello, {user.username}</h2>
+      <button onClick={signOut}>Sign Out</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- zenith/src/index.js
+++ zenith/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +17,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+Amplify.configure(awsExports);
+ReactDOM.render(<App />, document.getElementById("root"));
